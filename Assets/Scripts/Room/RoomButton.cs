bff3ff4996f4da5445767beeeceebb446f819ba8
Duong Van Huy <huydv@rikkeisoft.com>
2019-04-23 17:40:27 +0700

    ui

---
+++ Assets/Scripts/Room/RoomButton.cs
@@ -0,0 +1,28 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomButton : MonoBehaviour {
+
+    public string roomName;
+    public int countPlayer;
+    public Text countPlayerUI;
+
+   
+	void Start () {
+		
+	}
+	
+	public void SetInformation(string name, int count)
+    {
+        roomName = name;
+        countPlayer = count;
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+}
---
+++ Assets/Scripts/Room/RoomManager.cs
@@ -0,0 +1,37 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class RoomManager : Photon.Monobehaviour
+{
+
+    public GameObject roomPrefab;
+    public string gameVersion = "1.0";
+    void Awake()
+    {
+       PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    void Start () {
+        Connect();
+    }
+
+    public void Connect()
+    {
+        // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
+        if (PhotonNetwork.IsConnected)
+        {
+        }
+        else
+        {
+            // #Critical, we must first and foremost connect to Photon Online Server.
+            PhotonNetwork.GameVersion = gameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
