1ccfd2c1604aaef183e61d3598a7a0dc5ef08122
Duong Van Huy <huydv@rikkeisoft.com>
2019-05-17 17:23:35 +0700

    camera

--- Assets/Scripts/Player/CameraControl.cs
+++ Assets/Scripts/Player/CameraControl.cs
@@ -8,24 +8,24 @@ public class CameraControl : MonoBehaviour {
     public Transform camera;
     public Transform player;
     public bool isPlay = true;
-	void Start () {
+    public float yRot;
+    public float xRot;
+
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (isPlay) {
-            float yRot = Input.GetAxis("Mouse X") * XSensitivity;
-            float xRot = Input.GetAxis("Mouse Y") * YSensitivity;
-            Debug.Log("xRot" + xRot);
+            yRot = Input.GetAxis("Mouse X") * XSensitivity;
+            xRot = Input.GetAxis("Mouse Y") * YSensitivity;
+
             player.Rotate(0, yRot, 0);
-            if (xRot == 0)
-                return;
-            if (xRot > -0.1f)
-                xRot = -0.1f;
-            if (xRot < 0.9f)
-                xRot = 0.9f;
             camera.Rotate(xRot, 0, 0);
+            Debug.Log(camera.rotation.eulerAngles);
+            if (camera.rotation.eulerAngles.x > 100 || camera.rotation.eulerAngles.x < 98)
+                camera.Rotate(-xRot, 0, 0);
         }
 	}
 }
